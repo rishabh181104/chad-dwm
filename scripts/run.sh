7a20e59198926237f08567f7c0570a2d772dd7cc
rishabh181104 <rishabhhaldiya18@gmail.com>
2024-10-18 21:52:32 +0530

    some changes

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-xrdb merge ~/.Xresources 
-xbacklight -set 10 &
-feh --bg-fill ~/Pictures/wall/gruv.png &
-xset r rate 200 50 &
+xrandr --output eDP-1 --mode "1920x1080" --rate 60.01
+
+~/.config/chadwm/chadwm/slstatus/./slstatus &
+
+feh --randomize --bg-fill ~/Wallpapers/*
+
 picom &
 
+dunst &
+
+flameshot &
+
 dash ~/.config/chadwm/scripts/bar.sh &
+
 while type chadwm >/dev/null; do chadwm && continue || break; done
